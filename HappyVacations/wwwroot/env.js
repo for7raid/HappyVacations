ee5707af4f2ad65a72f4b10c6a6942cdb23c014c
for7raid <alexey@for7raid.ru>
2023-11-15 19:16:47 +0300

    move conf file

---
+++ HappyVacations/wwwroot/env.js
@@ -0,0 +1,8 @@
+﻿export const firebaseConfig = {
+    apiKey: "AIzaSyA4G8UE1E4noKHoG4uORUhd7aUS9XUmX-4",
+    authDomain: "happy-vacation-test.firebaseapp.com",
+    projectId: "happy-vacation-test",
+    storageBucket: "happy-vacation-test.appspot.com",
+    messagingSenderId: "859562352554",
+    appId: "1:859562352554:web:ccaa4478df6d638fec15d3"
+};
