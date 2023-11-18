f9d9363f2ed286890d3b440f9dc1458191b52777
for7raid <alexey@for7raid.ru>
2023-11-18 20:14:28 +0300

    Роль сотрудника в выпадающий список

---
+++ HappyVacations/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HappyVacations.Models;
+
+namespace HappyVacations.ViewModels
+{
+    public class EmployeeEditViewModel
+    {
+        public Employee Item { get; init; } = default!;
+        public IEnumerable<string> Roles { get; init; }
+    }
+}
